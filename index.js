06088129dad28f6141cb76f3fa76f3ad77c3cd2d
Vignesh Jagadeesh <vignesh@pipemonk.com>
2016-05-13 12:37:53 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,42 @@
+/**
+ * Created by zapstitch on 13/5/16.
+ */
+'use strict';
+
+var Promise = require('bluebird');
+var _ = require('lodash');
+
+module.exports = function (options) {
+  const seneca = this;
+  const winston = options.winston;
+  delete seneca.fixedargs.fatal$;
+
+  seneca.decorate('fire', (cmd, args,test) => {
+    console.log(cmd,args,test);
+  return new Promise(function (resolve, reject) {
+
+    seneca.act(cmd, args, (err, result) => {
+      if (err) {
+        seneca.log.info('Seneca-sanitize error firing:', args, '\n\n', err);
+        reject(err);
+      }
+
+      if (result.success) {
+      return result.payload ? resolve(result.payload) : resolve();
+    } else {
+
+      let error = new Error(result.error.errorMessage);
+      _.extend(error,result.error);
+      return reject(error);
+    }
+
+  })
+  });
+
+});
+
+  return {
+    name: 'errorTransform'
+  }
+
+};
